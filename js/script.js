a908283bc5408b92b3d3fcf915681108a8ce8d70
YJH1218 <caju1218@gmail.com>
2022-04-06 12:55:52 +0900

    04.06.up2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
+    // 슬라이드 1
     $('.slider-wrap01 > .slider').slick({
     autoplay: 4000,
-
+    arrows: false,
     });
+    // 슬라이드 2
     $('.slider-wrap02 > .slider').slick({
       vertical: true,
       verticalSwiping: true,
       autoplay: 3000,
     })
-
+    // 슬라이드 3
     $('.section02 > .slider-wrap03 >.slider').slick({
       slidesToShow: 4,
       SlidesToScroll: 1,
-      
+      swipeToSlide: true,
+      infinite: false,
+      prevArrow: $(".section02 > .slider-wrap03 > .prev-btn") ,
+      nextArrow: $(".section02 > .slider-wrap03 > .next-btn") ,
     })
+
+    // $('.section02 > .slider-wrap03 > .prev-btn').slick('slickPrev');
+    // $('.section02 > .slider-wrap03 > .next-btn').slick('slickNext');
 
   });
